5d286980b761af7e998dd1f21932f7e020f3b932
Olha <olya.shutileva@gmail.com>
2026-04-07 23:59:17 +0300

    added examples from 1-st lesson

--- src/main/java/com/softserve/academy/module1/DialogExample.java
+++ src/main/java/com/softserve/academy/module1/DialogExample.java
@@ -2,12 +2,21 @@ package com.softserve.academy.module1;
 
 import java.util.Scanner;
 
+/**
+ * The DialogExample class is a simple Java program that demonstrates a user-interactive
+ * console application using standard input and output. It prompts the user to enter
+ * their name and age and then displays a personalized greeting and acknowledgment.
+ *
+ * The program makes use of the Scanner class to read input from the console.
+ * It performs the following steps:
+ * 1. Greets the user and asks for their name.
+ * 2. Prompts the user to enter their age.
+ * 3. Prints a personalized greeting and a remark about their age.
+ *
+ * This class serves as an introduction to basic console I/O operations in Java.
+ */
 public class DialogExample {
     public static void main(String[] args) {
-
-        double pi = Math.PI;
-        double perimeter = pi * 10;
-        System.out.println(perimeter);
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! \nWhat is your name?");
