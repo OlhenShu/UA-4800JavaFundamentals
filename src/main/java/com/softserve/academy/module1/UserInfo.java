f5912d63166d780fbbe1f74c9cad8a87f68184db
Ihor Vavryk <ihor.vavryk1@gmail.com>
2026-04-08 18:17:52 +0300

    fix rUserInfo

--- src/main/java/com/softserve/academy/module1/UserInfo.java
+++ src/main/java/com/softserve/academy/module1/UserInfo.java
@@ -2,7 +2,7 @@ package com.softserve.academy.module1;
 
 import java.util.Scanner;
 
-class rUserInfo {
+class UserInfo {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
